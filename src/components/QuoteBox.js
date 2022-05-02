560d2d06ddc1cee575dfb930dc1d592150fa35f6
Raizen-7 <ferminyanes07@gmail.com>
2022-05-01 23:16:39 -0500

    App done

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -1,23 +1,22 @@
 import {React,  useState } from 'react';
-import phrares from '../phrases.json';
+import quotes from '../quotes.json';
+import colors from './add-on';
 
 /*Funcion para llamado random de las Frases*/
 
-const getRandom = () => Math.floor(Math.random() * phrares.length);
+const getRandom = () => Math.floor(Math.random() * quotes.length);
 
-/*arreglo de colores para fondo, letras y botón*/
-const colors = ['#845EC2', '#D65DB1', '##008F7A', '#FF9671', '#FFC75F', '#0081CF', '#C34A36']
 
 /*componente funcional */
 const QuoteBox = () => {
 
-    const [ phrare, setPhrare ] = useState( phrares[getRandom()]);
+    const [ phrare, setPhrare ] = useState( quotes[getRandom()]);
     console.log(phrare)
 
     const changePhrase = () => { 
-        setPhrare(phrares[getRandom()]);
+        setPhrare(quotes[getRandom()]);
     }
-    /*Constante para guardar llamado random de las los colores*/
+    /*Constante para guardar llamado random de los colores*/
     const color = colors[Math.floor(Math.random() * colors.length)];
 
     document.body.style = `background:${color}`;
---
+++ src/components/add-on.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+/*arreglo de colores para fondo, letras y botón*/
+const colors = ['#845EC2', '#D65DB1', '##008F7A', '#FF9671', '#FFC75F', '#0081CF', '#C34A36']
+
+export default colors;
