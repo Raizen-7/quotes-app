8da59b5560e2cbfa1a21f1df2ccca13e1eb8f32f
Raizen-7 <ferminyanes07@gmail.com>
2022-05-03 22:52:26 -0500

    Code validation done

--- src/components/add-on.js
+++ src/components/add-on.js
@@ -1,4 +1,4 @@
-import React from 'react';
+
 
 /*arreglo de colores para fondo, letras y botón*/
 const colors = ['#845EC2', '#D65DB1', '##008F7A', '#FF9671', '#FFC75F', '#0081CF', '#C34A36', '#69fae5', '#800816', '#66035c', '#3a1070', '#1d6c85', '#00ff7b', '#03542a', '#3e4014' ]
